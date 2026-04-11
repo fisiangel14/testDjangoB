faacaada4b3da7cc98822d97dceeb822353bca93
Angel <fisiangel.14@gmail.com>
2026-04-11 00:57:18 -0500

    add models for practice orm

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)

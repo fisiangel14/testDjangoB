d0c938d6a07b2b3dbcf9308aadb5669179d1d4ed
Angel <fisiangel.14@gmail.com>
2026-04-12 12:14:10 -0500

    add orm querys

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,7 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('equipo/', views.equipo, name='equipo'),
+    path('equipo/<int:equipo_id>/jugadores/', views.jugadores_por_equipo, name='jugadores_por_equipo'),
     path('jugadores/', views.jugadores, name='jugadores'),
+    path('jugadores/delanteros/', views.jugadores_delanteros, name='jugadores_delanteros'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Equipo, Jugador
 
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
 
 def equipo(request):
-    return render(request, 'core/equipo.html')
+    equipos = Equipo.objects.all()
+    return render(request, 'core/equipo.html', {'equipos': equipos})
 
 def jugadores(request):
-    return render(request, 'core/jugadores.html')
+    #select * from Jugador
+    jugadores = Jugador.objects.all()
+    #for debugging
+    # print("Jugadores:", jugadores)  # Agrega esta línea para verificar los jugadores obtenidos  
+    # print("Número de jugadores:", jugadores.count())  # Agrega esta línea para verificar el número de jugadores obtenidos
+    return render(request, 'core/jugadores.html', {'jugadores': jugadores})
+
+def jugadores_por_equipo(request, equipo_id):
+    equipo = get_object_or_404(Equipo, id=equipo_id)
+    jugadores = Jugador.objects.filter(equipo=equipo)
+    return render(
+        request,
+        'core/jugadores.html',
+        {
+            'equipo': equipo,
+            'jugadores': jugadores
+        }
+    )
+
+def jugadores_delanteros(request):
+    jugadores = Jugador.objects.filter(posicion='DEL')  # Filtra jugadores por posición de delantero
+    return render(request, 'core/jugadores_delanteros.html', {'jugadores': jugadores})
